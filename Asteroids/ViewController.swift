da363801ffdc5cc09155d07cee740e61f247788b
Sean Hickey <shickey@blakeschool.org>
2016-12-05 17:33:44 -0600

    Initial Commit

---
+++ Asteroids/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Asteroids
+//
+//  Created by Sean Hickey on 12/5/16.
+//  Copyright © 2016 Sean Hickey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
